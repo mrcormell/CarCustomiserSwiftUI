730ab1851da9b982f5a6fb383e26ec87fac51be9
David Cormell <d.cormell@etoncollege.org.uk>
2020-12-14 11:08:22 +0000

    Initial Commit

---
+++ CarCustomiser/CarCustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomiserApp.swift
+//  CarCustomiser
+//
+//  Created by Cormell, David - DPC on 14/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarCustomiser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarCustomiser
+//
+//  Created by Cormell, David - DPC on 14/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
